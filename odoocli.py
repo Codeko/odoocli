463397cafa8ff6af38df002d8c8fbf91f85de743
psicobyte <psicobyte@gmail.com>
2020-03-16 11:43:35 +0100

    refs: #15598 Modificado para que trabaje como módulo

--- odoocli.py
+++ odoocli.py
@@ -12,37 +12,6 @@ from configparser import ConfigParser
 from datetime import datetime, date, timedelta
 from dotenv import load_dotenv
 
-config_file = [os.path.dirname(os.path.realpath(__file__)) + '/odoocli.conf']
-
-help_text = """
-Muestra un resumen de la jornada laboral registrada en Odoo hasta el momento,
-indicando los días y horas laborables totales del mes, las horas laborables
-hasta el día actual y las horas trabajadas hasta el momento (incluídas las
-correspondientes a la sesión actual, si es que hay una abierta por el usuario).
-"""
-epilog_text = """
-Si se indica un mes concreto con la opción --month, se mostrará el resumen
-total de ese mes. Se puede concretar el año con la opción --year.
-
-Si se usa el flag --list se mostrará un listado de asistencias en lugar del
-resumen.
-
-Con --file NOMBRE_DE ARCHIVO se guarará el listado de asistencias en un archivo
-en formato CSV.
-
-Si existen las variables de entorno "ODOOCLIUSER" y "ODOOCLIPASS", se usarán
-para el login en Odoo a menos que se indique un usuario con el argumento
---user.
-
-Si existe la variable "ODOOCLIUSER" pero no "ODOOCLIPASS", el programa tomará
-el usuario de "ODOOCLIUSER" y mostrará un prompt solicitando la contraseña.
-
-Si tampoco existe la variable "ODOOCLIUSER", se mostrarán sendos prompts
-solicitando el nombre de usuario y la contraseña.
-
-Si se usa el argumento --user el programa ignorará las variables de entorno y
-mostrará un prompt solicitando la contraseña.
-"""
 labor_hours_by_day = 7.0
 
 
@@ -105,7 +74,7 @@ def list_to_screen(login, month=None, year=None):
 
 ########################################################################
 #
-# Holidays, weekends and vancances
+# Holidays, weekends and vacations
 #
 ########################################################################
 
@@ -115,26 +84,26 @@ def public_holidays(login, year):
     Festivos de un año
     """
     holidays = login['conn'].execute_kw(
-                    login['db'],
-                    login['uid'],
-                    login['password'],
-                    'hr.holidays.public',
-                    'search_read',
-                    [[('year', '=', year)]],
-                    {'fields': ['line_ids']})
+        login['db'],
+        login['uid'],
+        login['password'],
+        'hr.holidays.public',
+        'search_read',
+        [[('year', '=', year)]],
+        {'fields': ['line_ids']})
     for i in holidays[0]['line_ids']:
         yield get_holiday(login, i)
 
 
 def get_holiday(login, id_holiday):
     holidays = login['conn'].execute_kw(
-                    login['db'],
-                    login['uid'],
-                    login['password'],
-                    'hr.holidays.public.line',
-                    'search_read',
-                    [[('id', '=', id_holiday)]],
-                    {})
+        login['db'],
+        login['uid'],
+        login['password'],
+        'hr.holidays.public.line',
+        'search_read',
+        [[('id', '=', id_holiday)]],
+        {})
     return holidays[0]
 
 
@@ -179,13 +148,13 @@ def get_vacances_by_month(login, month=None, year=None):
         month = int(datetime.now().month)
 
     vacances = login['conn'].execute_kw(
-                    login['db'],
-                    login['uid'],
-                    login['password'],
-                    'hr.holidays',
-                    'search_read',
-                    [[]],
-                    {})
+        login['db'],
+        login['uid'],
+        login['password'],
+        'hr.holidays',
+        'search_read',
+        [[]],
+        {})
     for i in vacances:
         if i['employee_id'] and i['employee_id'][0] == user_id:
             if i['date_from'] and i['date_to'] and i['state'] != 'refuse':
@@ -229,17 +198,20 @@ def get_user_attendance_by_month(login, month=None, year=None):
     if month is None:
         month = int(datetime.now().month)
     date_filter = "{}-{:02d}-%".format(year, month)
-    attendance = login['conn'].execute_kw(
-                    login['db'],
-                    login['uid'],
-                    login['password'],
-                    'hr.attendance',
-                    'search_read',
-                    [[('employee_id', '=', user_id), ('check_in', '=like', date_filter)]],
-                    {'fields': ['employee_id', 'check_in', 'check_out', 'worked_hours']})
-
-    for e in attendance:
-        yield e['check_in'], e['check_out'], e['worked_hours']
+    try:
+        attendance = login['conn'].execute_kw(
+            login['db'],
+            login['uid'],
+            login['password'],
+            'hr.attendance',
+            'search_read',
+            [[('employee_id', '=', user_id), ('check_in', '=like', date_filter)]],
+            {'fields': ['employee_id', 'check_in', 'check_out', 'worked_hours']})
+    except TypeError:
+        return None
+    else:
+        for e in attendance:
+            yield e['check_in'], e['check_out'], e['worked_hours']
 
 
 def count_labour_days(login, month=None, year=None):
@@ -307,7 +279,7 @@ def open_session_worked_hours(login):
     for e in get_user_attendance_by_month(login, month, year):
         if not e[1] and str_to_localtime(e[0]).tm_mday == today:
             delta = datetime.now() - datetime(*str_to_localtime(e[0])[:6])
-            return delta.total_seconds()/3600
+            return delta.total_seconds() / 3600
     return 0
 
 
@@ -402,96 +374,155 @@ def get_user_by_email(login):
     return None
 
 
+def get_mail_users(login):
+    """
+    Retorna los emails de todos los usuarios
+    """
+    users = login['conn'].execute_kw(login['db'],
+                                     login['uid'],
+                                     login['password'],
+                                     'res.users',
+                                     'search_read',
+                                     [],
+                                     {'fields': ['email']})
+    for user in users:
+        yield user['email']
+
+
+def bulk(login, function):
+    for user in get_mail_users(login):
+        new_login_data = login_data
+        new_login_data['user_email'] = user
+        if count_worked_hours(new_login_data):
+            print('Procesando', user)
+            function(new_login_data)
+        else:
+            print('Se omite', user)
+
+
 ########################################################################
 #
 # Main
 #
 ########################################################################
 
-load_dotenv()
+if __name__ == '__main__':
 
-config_parser = ConfigParser()
-config_parser.read(config_file)
-if config_parser.has_option('server', 'host') \
-                            and config_parser.has_option('server', 'database'):
-    server = config_parser.get('server', 'host')
-    db = config_parser.get('server', 'database')
-else:
-    sys.exit('Error en el archivo de configuración')
+    config_file = [os.path.dirname(os.path.realpath(__file__)) + '/odoocli.conf']
 
-parser = argparse.ArgumentParser(
-                        formatter_class=argparse.RawDescriptionHelpFormatter,
-                        description=help_text,
-                        epilog=epilog_text)
-parser.add_argument('-u', '--user', type=str, dest='user',
-                    help='Nombre de usuario.\nSi no se aporta se utilizará el \
-                         contenido en la variable de entorno "ODOOCLIUSER"')
-parser.add_argument('-m', '--month', type=int, dest='month',
-                    help='Número en el rango [1-12] indicando el mes del que \
-                         se mostrará el informe')
-parser.add_argument('-y', '--year', type=int, dest='year',
-                    help='Año del que se mostrará el informe.\nSi no se \
-                         indica el mes, el valor de este campo será ignorado')
-parser.add_argument('-f', '--file', type=str,
-                    help='Nombre del archivo en el que se guardará un listado \
-                         de asistencias (parecido al mostrado con --list) en \
-                         formato CSV\nEste argumento hace que se ignore la \
-                         opción --list')
-parser.add_argument('-l', '--list', action='count',
-                    help='Muestra una lista de asistencias en lugar del \
-                         resumen')
-parser.add_argument('-e', '--email', type=str, dest='email',
-                    help='eMail del usuario del que se generará el informe')
-args = parser.parse_args()
+    help_text = """
+    Muestra un resumen de la jornada laboral registrada en Odoo hasta el momento,
+    indicando los días y horas laborables totales del mes, las horas laborables
+    hasta el día actual y las horas trabajadas hasta el momento (incluídas las
+    correspondientes a la sesión actual, si es que hay una abierta por el usuario).
+    """
+    epilog_text = """
+    Si se indica un mes concreto con la opción --month, se mostrará el resumen
+    total de ese mes. Se puede concretar el año con la opción --year.
 
-if args.user:
-    username = args.user
-    password = getpass.getpass()
-elif os.environ.get('ODOOCLIUSER') and not os.environ.get('ODOOCLIPASS'):
-    username = os.environ['ODOOCLIUSER']
-    password = getpass.getpass()
-elif os.environ.get('ODOOCLIUSER') and os.environ.get('ODOOCLIPASS'):
-    username = os.environ['ODOOCLIUSER']
-    password = os.environ['ODOOCLIPASS']
-else:
-    username = input('Username: ')
-    password = getpass.getpass()
+    Si se usa el flag --list se mostrará un listado de asistencias en lugar del
+    resumen.
 
-common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(server))
-uid = common.authenticate(db, username, password, {})
+    Con --file NOMBRE_DE ARCHIVO se guarará el listado de asistencias en un archivo
+    en formato CSV.
 
-if uid:
-    login_data = {'db': db, 'password': password, 'username': username, 'uid': uid,
-                  'conn': xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(server))}
-    if args.email:
-        login_data['user_email'] = args.email
-else:
-    sys.exit('Error en el Login')
+    Si existen las variables de entorno "ODOOCLIUSER" y "ODOOCLIPASS", se usarán
+    para el login en Odoo a menos que se indique un usuario con el argumento
+    --user.
 
-if args.month and (args.month < 1 or args.month > 12):
-    sys.exit("Mes fuera de rango")
+    Si existe la variable "ODOOCLIUSER" pero no "ODOOCLIPASS", el programa tomará
+    el usuario de "ODOOCLIUSER" y mostrará un prompt solicitando la contraseña.
 
-if args.file:
-    if args.month:
-        if args.year:
-            list_to_csv(login_data, args.file, args.month, args.year)
-        else:
-            list_to_csv(login_data, args.file, args.month)
+    Si tampoco existe la variable "ODOOCLIUSER", se mostrarán sendos prompts
+    solicitando el nombre de usuario y la contraseña.
+
+    Si se usa el argumento --user el programa ignorará las variables de entorno y
+    mostrará un prompt solicitando la contraseña.
+    """
+
+    load_dotenv()
+
+    config_parser = ConfigParser()
+    config_parser.read(config_file)
+    if config_parser.has_option('server', 'host') \
+            and config_parser.has_option('server', 'database'):
+        server = config_parser.get('server', 'host')
+        db = config_parser.get('server', 'database')
     else:
-        list_to_csv(login_data, args.file)
-elif args.list:
-    if args.month:
-        if args.year:
-            list_to_screen(login_data, args.month, args.year)
-        else:
-            list_to_screen(login_data, args.month)
+        sys.exit('Error en el archivo de configuración')
+
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=help_text,
+        epilog=epilog_text)
+    parser.add_argument('-u', '--user', type=str, dest='user',
+                        help='Nombre de usuario.\nSi no se aporta se utilizará el \
+                             contenido en la variable de entorno "ODOOCLIUSER"')
+    parser.add_argument('-m', '--month', type=int, dest='month',
+                        help='Número en el rango [1-12] indicando el mes del que \
+                             se mostrará el informe')
+    parser.add_argument('-y', '--year', type=int, dest='year',
+                        help='Año del que se mostrará el informe.\nSi no se \
+                             indica el mes, el valor de este campo será ignorado')
+    parser.add_argument('-f', '--file', type=str,
+                        help='Nombre del archivo en el que se guardará un listado \
+                             de asistencias (parecido al mostrado con --list) en \
+                             formato CSV\nEste argumento hace que se ignore la \
+                             opción --list')
+    parser.add_argument('-l', '--list', action='count',
+                        help='Muestra una lista de asistencias en lugar del \
+                             resumen')
+    parser.add_argument('-e', '--email', type=str, dest='email',
+                        help='eMail del usuario del que se generará el informe')
+    args = parser.parse_args()
+
+    if args.user:
+        username = args.user
+        password = getpass.getpass()
+    elif os.environ.get('ODOOCLIUSER') and not os.environ.get('ODOOCLIPASS'):
+        username = os.environ['ODOOCLIUSER']
+        password = getpass.getpass()
+    elif os.environ.get('ODOOCLIUSER') and os.environ.get('ODOOCLIPASS'):
+        username = os.environ['ODOOCLIUSER']
+        password = os.environ['ODOOCLIPASS']
     else:
-        list_to_screen(login_data)
-else:
-    if args.month:
-        if args.year:
-            show_resume(login_data, args.month, args.year)
-        else:
-            show_resume(login_data, args.month)
+        username = input('Username: ')
+        password = getpass.getpass()
+
+    common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(server))
+    uid = common.authenticate(db, username, password, {})
+
+    if uid:
+        login_data = {'db': db, 'password': password, 'username': username, 'uid': uid,
+                      'conn': xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(server))}
+        if args.email:
+            login_data['user_email'] = args.email
     else:
-        show_resume_now(login_data)
+        sys.exit('Error en el Login')
+
+    if args.month and (args.month < 1 or args.month > 12):
+        sys.exit("Mes fuera de rango")
+    if args.file:
+        if args.month:
+            if args.year:
+                list_to_csv(login_data, args.file, args.month, args.year)
+            else:
+                list_to_csv(login_data, args.file, args.month)
+        else:
+            list_to_csv(login_data, args.file)
+    elif args.list:
+        if args.month:
+            if args.year:
+                list_to_screen(login_data, args.month, args.year)
+            else:
+                list_to_screen(login_data, args.month)
+        else:
+            list_to_screen(login_data)
+    else:
+        if args.month:
+            if args.year:
+                show_resume(login_data, args.month, args.year)
+            else:
+                show_resume(login_data, args.month)
+        else:
+            show_resume_now(login_data)
